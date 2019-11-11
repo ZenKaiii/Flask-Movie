37165152eac41dfc00b23335f06880da374b87a6
zenkaiii <wintersoul@foxmail.com>
2019-11-11 11:02:05 +0800

    前台搭建完成

--- app/home/views.py
+++ app/home/views.py
@@ -2,10 +2,10 @@
 from . import home
 from flask import render_template,redirect,url_for
 
-
-@home.route("/")
-def index():
-    return render_template("home/index.html")
+#
+# @home.route("/")
+# def index():
+#     return render_template("home/index.html")
 
 @home.route("/login/")
 def login():
@@ -38,3 +38,20 @@ def loginlog():
 @home.route("/moviecol/")
 def moviecol():
     return render_template("home/moviecol.html")
+
+@home.route("/")
+def index():
+    return render_template("home/index.html")
+
+@home.route("/animation/")
+def animation():
+    return render_template("home/animation.html")
+
+@home.route("/search/")
+def search():
+    return render_template("home/search.html")
+
+@home.route("/play/")
+def play():
+    return render_template("home/play.html")
+
