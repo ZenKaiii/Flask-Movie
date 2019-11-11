e4fd6a6768cb2ebc03342c45e36e746830256596
zenkaiii <wintersoul@foxmail.com>
2019-11-11 21:33:06 +0800

    管理员前台

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,6 +1,15 @@
 #coding:utf8
 from . import admin
+from flask import render_template,redirect,url_for
 
 @admin.route("/")
 def index():
-    return "<h1 style='color:red'>this is admin</h1>"
+    return render_template("admin/index.html")
+
+@admin.route("/login/")
+def login():
+    return render_template("admin/login.html")
+
+@admin.route("/logout/")
+def logout():
+    return redirect(url_for("admin.login"))
